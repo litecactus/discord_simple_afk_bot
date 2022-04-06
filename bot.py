f93425c2e51befc6921f0dbfe85098572bcba590
Alex <5160835+litecactus@users.noreply.github.com>
2022-04-06 10:37:26 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
-
+global x
+x = "[AFK] "
 client = commands.Bot(command_prefix = '.')
 
 @client.event
@@ -9,15 +10,29 @@ async def on_ready():
 
 @client.command(pass_context=True)
 async def afk(ctx):
-    x = "[AFK] "
+    global x
+    global z
     current_nick = ctx.author.nick
     if x in current_nick:
-        old_nick = current_nick[6:]
+        old_nick = current_nick.replace(x,"")
         await ctx.author.edit(nick=old_nick)
     else:
-         await ctx.author.edit(nick="[AFK] "+current_nick)
+         await ctx.author.edit(nick=x+current_nick)
     await ctx.channel.purge(limit=1)
-    
-    
 
-client.run('enter_your_token')
+
+client.run('enter token here')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
